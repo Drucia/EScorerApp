e1a9f8e6f3c870f41cd0b83874bc68cf63db260d
Ola Druciak <ola.druciak@wp.pl>
2019-09-25 19:27:25 +0200

    Added main navigation in main panel

--- EScorerApp/app/src/main/java/com/druciak/escorerapp/activities/MainPanelActivity.java
+++ EScorerApp/app/src/main/java/com/druciak/escorerapp/activities/MainPanelActivity.java
@@ -1,16 +1,89 @@
 package com.druciak.escorerapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.druciak.escorerapp.R;
+import com.druciak.escorerapp.ui.login.LoginActivity;
+import com.druciak.escorerapp.ui.login.LoginViewModel;
+import com.druciak.escorerapp.ui.login.LoginViewModelFactory;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainPanelActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_panel);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_user_data, R.id.nav_matches,
+                R.id.nav_tools)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_panel, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.action_logout:
+                logoutUser();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logoutUser() {
+        loginViewModel.logout();
+        startActivity(new Intent(this, LoginActivity.class));
+        Toast.makeText(this, "Nastąpiło wylogowanie", Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void onClickedExit(MenuItem item) {
+        loginViewModel.logout();
+        startActivity(new Intent(this, GoodbyeActivity.class));
+        finish();
     }
 }
